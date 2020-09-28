ecc1b1a1691bee2c28d94581f6389776c732efbe
Johanna Simfors <jsimfors@gmail.com>
2020-09-28 17:06:51 +0200

    contact div updated

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 import "./styles.css"
 import { Row, Col} from 'react-bootstrap';
 import contactpic from "../../imgs/contact.png"
+import linklogo from "../../imgs/linklogo.png"
+import githublogo from "../../imgs/gitlogo.png"
+import emaillogo from "../../imgs/maillogo.png"
+
 
 
 function Contact() {
@@ -10,9 +14,17 @@ return (
         <Row>
             <Col  sm={{ span: 8, offset: 2 }} >
             <img src={contactpic} alt="concactpic" id="contactImg"/>
-            <br/>
-            Contact info:
-            Name: Johanna
+            <div className="contactLogos">
+                <a href="https://www.linkedin.com/in/johanna-simfors-247313160/">
+                    <img src={linklogo} alt="linklogo"/>
+                </a>
+                <a href="https://github.com/jsimfors/">
+                    <img src={githublogo} alt="gitlogo"/>
+                </a>
+                <a href="https://www.linkedin.com/in/johanna-simfors-247313160/">
+                    <img src={emaillogo} alt="maillogo"/>
+                </a>
+            </div>
             </Col>
         </Row>
 
