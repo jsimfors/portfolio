b4f3e4e736469b15d1cf1c71418cb687cfc7b03d
Johanna Simfors <jsimfors@gmail.com>
2020-09-29 20:57:45 +0200

    stylefix

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -10,15 +10,18 @@ function About() {
 return (
     <div className="about-container" id="section2">
         <Row>
-            <Col sm={{ span: 4, offset: 1 }} className="image-outer-container">
+            <Col sm={{ span: 3, offset: 2 }} className="image-outer-container">
                 <div className="image-container"></div>
                 {/* <img src={profpic} alt="profpic" id="profImg"/> */}
             </Col>
             <Col sm={5} className="text-container">
                 <img src={aboutpic} alt="profpic" id="aboutImg"/>
                 <br/>
-                <p>Hello about me bla bla bla
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu. 
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Hello about me bla bla bla Lorem ipsum dolor sit amet, 
+                <br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;consectetur adipiscing elit, sed do eiusmod tempor 
+                <br/>&nbsp;&nbsp;&nbsp;&nbsp;ut labore et dolore magna aliqua. Ut enim ad minim, 
+                <br/>&nbsp;&nbsp;quis nostrud exercitationullamco laboris nisi blaisi.
+                <br/>adipiscing elit, sed do eiusmod elit brol.
                 </p>
             </Col>
         </Row>
